7e19201604ce81c570f241771025f50addb176db
brijkishorsoni1210 <84934809+brijkishorsoni1210@users.noreply.github.com>
2022-08-02 10:47:17 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,35 @@
+<!-- <?php
+// $db_name = "testing";
+// try {
+//     // connect to OVHcloud Public Cloud Databases for MongoDB (cluster in version 4.4, MongoDB PHP Extension in 1.8.1)
+//     $m = new MongoClient("mongodb://localhost:38015");
+//     echo "Connection to database successfully";
+//     // display the content of the driver, for diagnosis purpose
+
+//     $mongo_db =  $m->$db_name;
+//     $mongo_collection = $mongo_db->users;
+    
+//     var_dump($mongo_db);
+
+//     $arr = array();
+//     $arr['user_id'] = '1234';
+//     $arr['user_name'] = 'ram';
+//     $mongo_collection->insert($arr);
+
+    
+
+// }
+// catch (Throwable $e) {
+//     // catch throwables when the connection is not a success
+//     echo "Captured Throwable for connection : " . $e->getMessage() . PHP_EOL;
+// }
+
+// $connection_str = ""
+
+
+
+
+
+
+
+?> -->
